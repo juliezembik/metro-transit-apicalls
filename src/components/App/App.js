0cddd73205c6dfd4b642106a86dc96a2db3a427c
Julie Zembik <kurokurosan@gmail.com>
2019-08-16 11:53:35 -0500

    emptied readme to add title

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import BusDom from '../BusDom/BusDom.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BusDom />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BusDom/BusDom.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class BusDom extends Component {
+    render () {
+        return (
+            <div>
+                Test goes here.
+            </div>
+        );
+    }
+};
+
+export default BusDom;
